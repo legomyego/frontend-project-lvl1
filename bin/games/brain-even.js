ece1032d4cc03b74ae714173c8ff9858016e319d
Dmitry Nikiforov <nikiforov@uzdevumi.lv>
2020-10-29 20:47:59 +0200

    task 5

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,31 +1,41 @@
 import promptly from 'promptly';
-import greeting from '../../src/index.js';
+import getRandomNumber from '../utils.js';
 
-const getRandomNumber = () => Math.floor(Math.random() * 101);
+const description = () => console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+async function greeting() {
+  console.log('Welcome to the Brain Games!');
+  const name = await promptly.prompt('May I have your name?');
+  console.log(`Hello, ${name}!`);
+  return name;
+}
+
+const rounds = 3;
 let correctAnswersCounter = 0;
 
-async function game() {
-  greeting();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  while (correctAnswersCounter < 3) {
-    const randomNumber = getRandomNumber();
-    console.log(`Question: ${randomNumber}`);
-
-    // eslint-disable-next-line no-await-in-loop
-    const answer = await promptly.prompt('Your answer:');
-
-    if (answer === 'yes' && randomNumber % 2 === 0) {
-      console.log('Correct!');
-      correctAnswersCounter += 1;
-    } else if (answer === 'no' && randomNumber % 2 !== 0) {
-      console.log('Correct!');
-      correctAnswersCounter += 1;
-    } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.`);
-      break;
-    }
+async function gameProcess(name) {
+  if (rounds === correctAnswersCounter) {
+    return console.log(`Congratulations, ${name}`);
   }
+
+  const randomNumber = getRandomNumber();
+  console.log(`Question: ${randomNumber}`);
+  const answer = await promptly.prompt('Your answer:');
+
+  if ((answer === 'yes' && randomNumber % 2 === 0) || (answer === 'no' && randomNumber % 2 !== 0)) {
+    console.log('Correct!');
+  } else {
+    return console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.`);
+  }
+
+  correctAnswersCounter += 1;
+  return gameProcess(name);
+}
+
+async function game() {
+  const name = await greeting();
+  description();
+  gameProcess(name);
 }
 
 game();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import promptly from 'promptly';
-
-export default async function greeting() {
-  console.log('Welcome to the Brain Games!');
-  const name = await promptly.prompt('May I have your name?');
-  console.log(`Hello, ${name}!`);
-}
