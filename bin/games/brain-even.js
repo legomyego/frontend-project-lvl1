5992930d2126b9b5308d5d2aa5cbc1772515bfe4
D.N <legomyego2005@gmail.com>
2020-11-14 20:04:03 +0200

    splitting engine

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,41 +1,17 @@
-import promptly from 'promptly';
 import getRandomNumber from '../utils.js';
-import getGreeting from '../../src/index.js';
+import runGame from '../../src/index.js';
 
-export default function getDescription() {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-}
+const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (value) => value % 2 === 0;
 
-const rounds = 3;
-let correctAnswersCounter = 0;
-
-async function gameProcess(name) {
+const generateGameData = () => {
   const question = getRandomNumber(0, 100);
   const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return {
+    question,
+    correctAnswer,
+  };
+};
 
-  console.log(`Question: ${question}`);
-  const userAnswer = await promptly.prompt('Your answer:');
-
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-    correctAnswersCounter += 1;
-  } else {
-    return console.log(`${userAnswer} is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet 's try again, ${name}!`);
-  }
-
-  if (rounds === correctAnswersCounter) {
-    return console.log(`Congratulations, ${name}`);
-  }
-
-  return gameProcess(name);
-}
-
-async function runGame() {
-  const name = await getGreeting();
-  getDescription();
-  gameProcess(name);
-}
-
-runGame();
+runGame(gameDescription, generateGameData);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import promptly from 'promptly';
 
-export default async function getGreeting() {
+const rounds = 3;
+let correctAnswersCounter = 0;
+
+async function getGreeting() {
   console.log('Welcome to the Brain Games!');
   const name = await promptly.prompt('May I have your name?');
   console.log(`Hello, ${name}!`);
   return name;
+}
+
+export default async function runGame(rule, gameData) {
+  const name = await getGreeting();
+  console.log(rule);
+  async function gameProcess() {
+    const { question, correctAnswer } = gameData();
+    console.log(`Question: ${question}`);
+    const userAnswer = await promptly.prompt('Your answer:');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswersCounter += 1;
+    } else {
+      return console.log(`${userAnswer} is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet 's try again, ${name}!`);
+    }
+
+    if (rounds === correctAnswersCounter) {
+      return console.log(`Congratulations, ${name}`);
+    }
+
+    return gameProcess(name);
+  }
+  gameProcess(name);
 }
